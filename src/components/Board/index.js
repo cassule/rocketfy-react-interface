a66e41131cc9d942cfe51fa8fa6db1282631cef7
Josué Francisco <josuefrancisco2017@outlook.pt>
2019-09-15 13:32:31 +0100

    finalizando app

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -8,20 +8,29 @@ import { Container } from "./styles";
 
 import List from "../List";
 
+import produce from "immer";
+
 const data = loadLists();
 
 export default function Board() {
   const [lists, setLists] = useState(data);
 
-  function move(from, to) {
-    console.log(from, to);
+  function move(fromList, toList, from, to) {
+    setLists(
+      produce(lists, draft => {
+        const dragged = draft[fromList].cards[from];
+
+        draft[fromList].cards.splice(from, 1);
+        draft[toList].cards.splice(to, 0, dragged);
+      })
+    );
   }
 
   return (
     <BoardContext.Provider value={{ lists, move }}>
       <Container>
-        {lists.map(list => (
-          <List key={list.title} data={list} />
+        {lists.map((list, index) => (
+          <List key={list.title} data={list} index={index} />
         ))}
       </Container>
     </BoardContext.Provider>
