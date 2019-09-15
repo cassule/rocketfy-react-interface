7bb1fbb6b0a6527dc6b30d624120289a561352ea
Josué Francisco <josuefrancisco2017@outlook.pt>
2019-09-15 07:39:56 +0100

    configuração base do projeto, adicionando styled-componets

---
+++ src/components/Card/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Container, Label } from "./styles";
+
+export default function Card() {
+  return (
+    <Container>
+      <header>
+        <Label color="#7159c1" />
+      </header>
+      <p>Fazer a migração completa do servidor.</p>
+      <img
+        src="https://api.adorable.io/avatars/183/johnny-english.png"
+        alt="Johnny English"
+      />
+    </Container>
+  );
+}
